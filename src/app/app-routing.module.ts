c81f1e24cc1f0edf972b392975ab2fb48892c361
itsadeki <itsadeki@gmail.com>
2018-11-05 13:06:13 +0100

    add mdia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TraversingComponent } from './traversing/traversing.component';
+import { MediaComponent } from './media/media.component';
 import { Erreur404Component } from './erreur404/erreur404.component';
 
 const routes: Routes = [
   { path: '', component: TraversingComponent },
+  { path: 'media', component: MediaComponent },
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '**', component: Erreur404Component }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './material-module';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,12 +11,14 @@ import { AppComponent } from './app.component';
 
 import { Erreur404Component } from './erreur404/erreur404.component';
 import { TraversingComponent } from './traversing/traversing.component';
+import { MediaComponent } from './media/media.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TraversingComponent,
     Erreur404Component,
+    MediaComponent,
 
   ],
   imports: [
