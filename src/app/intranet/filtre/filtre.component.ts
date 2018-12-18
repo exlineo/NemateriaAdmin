295d419ed0f027d952ad6d4198fcc8b18a5e1319
itsadeki <itsadeki@gmail.com>
2018-12-18 15:00:58 +0100

    change: integration sur http://localhost:4200/intranet/filtres

--- src/app/intranet/filtre/filtre.component.ts
+++ src/app/intranet/filtre/filtre.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { useAnimation, transition, trigger, style, animate, state } from '@angular/animations';
 
 @Component({
-  selector: 'app-filtre',
-  templateUrl: './filtre.component.html',
-  styleUrls: ['./filtre.component.css']
+	selector: 'app-filtre',
+	templateUrl: './filtre.component.html',
+	styleUrls: ['./filtre.component.css'],
+	animations: [
+		trigger('openCloseLeftPanel', [
+			state('open', style({
+				width: '200px'
+			})),
+			state('closed', style({
+				width: '0'
+			})),
+			transition('open => closed', [
+				animate('0.5s')
+			]),
+			transition('closed => open', [
+				animate('0.5s')
+			]),
+		]),
+		trigger('openCloseRightPanel', [
+			state('open', style({
+				width: '564px'
+			})),
+			state('closed', style({
+				width: '0'
+			})),
+			transition('open => closed', [
+				animate('0.5s')
+			]),
+			transition('closed => open', [
+				animate('0.5s')
+			]),
+		]),
+	],
 })
 export class FiltreComponent implements OnInit {
 
-  constructor() { }
+	leftPanelIsOpen = true;
+	rightPanelIsOpen = true;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+
+	toggleLeftPanel($event): void {
+		$event.preventDefault();
+		this.leftPanelIsOpen = !this.leftPanelIsOpen;
+	}
+
+	toggleRightPanel($event): void {
+		$event.preventDefault();
+		this.rightPanelIsOpen = !this.rightPanelIsOpen;
+	}
 
 }
