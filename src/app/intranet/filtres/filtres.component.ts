407f4dafaa616c4da8d37d8e72699dabedf33ff1
exlineo <gabbadie@step-sa.fr>
2019-01-24 23:35:09 +0100

    Uniformisation des présentations

--- src/app/intranet/filtres/filtres.component.ts
+++ src/app/intranet/filtres/filtres.component.ts
@@ -1,60 +1,101 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { useAnimation, transition, trigger, style, animate, state } from '@angular/animations';
+
+import { CONST } from '../systeme/const';
+import { FiltreModel } from '../systeme/modeles/filtre.modele';
+import { CollectionModel } from '../systeme/modeles/collection.modele';
+import { NoticeModel } from '../systeme/modeles/notice.modele';
+import { CollectionService } from '../systeme/services/collection.service';
 
 @Component({
 	selector: 'app-filtres',
 	templateUrl: './filtres.component.html',
-	styleUrls: ['./filtres.component.css'],
-	animations: [
-		trigger('openCloseLeftPanel', [
-			state('open', style({
-				width: '200px'
-			})),
-			state('closed', style({
-				width: '0'
-			})),
-			transition('open => closed', [
-				animate('0.5s')
-			]),
-			transition('closed => open', [
-				animate('0.5s')
-			]),
-		]),
-		trigger('openCloseRightPanel', [
-			state('open', style({
-				width: '564px'
-			})),
-			state('closed', style({
-				width: '0'
-			})),
-			transition('open => closed', [
-				animate('0.5s')
-			]),
-			transition('closed => open', [
-				animate('0.5s')
-			]),
-		]),
-	],
+	styleUrls: ['./filtres.component.css']
 })
 export class FiltresComponent implements OnInit {
+	filtresListe:Array<FiltreModel>;
+	collectionListe: CollectionModel[] = []; // Liste de toutes les collections
+	noticeListe: NoticeModel[] = []; // Liste des notices d'une sélection
+	notice:NoticeModel;
+	filtre: FiltreModel; // La collection sélectionnée à afficher
+	collectionAffiche: boolean = false;
 
-	leftPanelIsOpen = true;
-	rightPanelIsOpen = true;
+	filtreSerie:string=''; // Filtrer les notices d'une collection en fonction de sa série
 
-	constructor() { }
+	leftPanelIsOpen = true; // Etat d'ouverture du panneau de gauche
+	rightPanelIsOpen = true; // Etat d'ouverture du panneau de droite
+
+	constructor(public collectionService: CollectionService, private rendu:Renderer2) { }
 
 	ngOnInit() {
+		this.collectionService.readCollections().subscribe(
+			data => {
+				this.collectionListe = data;
+				this.collectionOnClick(0);
+			}
+		);
+	}
+	/**
+	 * Afficher le détail d'une collection
+	 * @param idCollection Id de la collection à afficher
+	 */
+	collectionOnClick(id): void {
+		this.filtre = this.filtresListe[id];
+		this.collectionAffiche = true;
+	}
+	noticeOnClick($event, idNotice): void {
+		$event.preventDefault();
+		alert('yolo');
 	}
 
-
+	/**
+	 * Ouvrir ou fermer le panneau de gauche
+	 * @param $event Evénement du clic
+	 */
 	toggleLeftPanel($event): void {
 		$event.preventDefault();
 		this.leftPanelIsOpen = !this.leftPanelIsOpen;
 	}
-
+	/**
+	 * Ouvrir ou fermer le panneau de droite
+	 * @param $event Evénement du clic
+	 */
 	toggleRightPanel($event): void {
 		$event.preventDefault();
 		this.rightPanelIsOpen = !this.rightPanelIsOpen;
 	}
+	/**
+	 * Afficher ou fermer une élément HTML
+	 * @param id Id de l'élément HTML a afficher
+	 */
+	toggleSeries(id){
+		let series = document.querySelector('#'+id);
+		console.log(series);
+		series.classList.toggle('ferme');
+	}
 
+	/* INTERACTIONS AVEC LES NOTICES */
+	// Afficher le composant avec le détail des infos sur la notice
+	noticeAfficheDetail($event, idNotice): void {
+		// $event.preventDefault();
+		this.notice = this.noticeListe[idNotice];
+		// this.afficheDetailNotice = !this.afficheDetailNotice;
+	}
+	
+	// Choisir une notice et la mettre dans la liste
+	noticeOnChoisi($event, idNotice): void {
+		$event.preventDefault();
+		this.notice = this.noticeListe[idNotice];
+		this.noticeListe[idNotice].selected = true;
+		if (this.noticeListe.indexOf(this.notice) == -1) {
+			this.noticeListe.push(this.notice);
+		}
+	}
+	// Afficher les infos d'une notice
+	noticeOnInfo($event, idNotice): void {
+		$event.preventDefault();
+		this.notice = this.noticeListe[idNotice];
+		
+		// this.noticeAffiche = true;
+	}
 }
--- src/app/intranet/scanner/scanner.component.ts
+++ src/app/intranet/scanner/scanner.component.ts
@@ -1,174 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  EventEmitter, Output  } from '@angular/core';
 
+import { CONST } from '../systeme/const';
 import { NoticeService } from "../systeme/services/notice.service";
 import { NoticeModel } from "../systeme/modeles/notice.modele";
+import { FiltrePipe } from "../systeme/pipes/filtre.pipe";
 import { useAnimation, transition, trigger, style, animate, state } from '@angular/animations';
-
-import { NestedTreeControl } from '@angular/cdk/tree';
-import { Injectable } from '@angular/core';
-import { MatTreeNestedDataSource } from '@angular/material/tree';
-import { BehaviorSubject } from 'rxjs';
-
-/**
- * Json node data with nested structure. Each node has a filename and a value or a list of children
- */
-export class FileNode {
-	children: FileNode[];
-	filename: string;
-	type: any;
-}
-
-/**
- * The Json tree data in string. The data could be parsed into Json object
- */
-const TREE_DATA = JSON.stringify({
-	Applications: {
-		Calendar: 'app',
-		Chrome: 'app',
-		Webstorm: 'app'
-	},
-	Documents: {
-		angular: {
-			src: {
-				compiler: 'ts',
-				core: 'ts'
-			}
-		},
-		material2: {
-			src: {
-				button: 'ts',
-				checkbox: 'ts',
-				input: 'ts'
-			}
-		}
-	},
-	Downloads: {
-		October: 'pdf',
-		November: 'pdf',
-		Tutorial: 'html'
-	},
-	Pictures: {
-		'Photo Booth Library': {
-			Contents: 'dir',
-			Pictures: 'dir'
-		},
-		Sun: 'png',
-		Woods: 'jpg'
-	}
-});
-
-/**
- * File database, it can build a tree structured Json object from string.
- * Each node in Json object represents a file or a directory. For a file, it has filename and type.
- * For a directory, it has filename and children (a list of files or directories).
- * The input will be a json object string, and the output is a list of `FileNode` with nested
- * structure.
- */
-@Injectable()
-export class FileDatabase {
-	dataChange = new BehaviorSubject<FileNode[]>([]);
-
-	get data(): FileNode[] { return this.dataChange.value; }
-
-	constructor() {
-		this.initialize();
-	}
-
-	initialize() {
-		// Parse the string to json object.
-		const dataObject = JSON.parse(TREE_DATA);
-
-		// Build the tree nodes from Json object. The result is a list of `FileNode` with nested
-		//     file node as children.
-		const data = this.buildFileTree(dataObject, 0);
-
-		// Notify the change.
-		this.dataChange.next(data);
-	}
-
-	/**
-	 * Build the file structure tree. The `value` is the Json object, or a sub-tree of a Json object.
-	 * The return value is the list of `FileNode`.
-	 */
-	buildFileTree(obj: { [key: string]: any }, level: number): FileNode[] {
-		return Object.keys(obj).reduce<FileNode[]>((accumulator, key) => {
-			const value = obj[key];
-			const node = new FileNode();
-			node.filename = key;
-
-			if (value != null) {
-				if (typeof value === 'object') {
-					node.children = this.buildFileTree(value, level + 1);
-				} else {
-					node.type = value;
-				}
-			}
-
-			return accumulator.concat(node);
-		}, []);
-	}
-}
+import { forEach } from '@angular/router/src/utils/collection';
+//import { toggleLeft } from '../systeme/library/animation';
 
 @Component({
 	selector: 'app-scanner',
 	templateUrl: './scanner.component.html',
-	styleUrls: ['./scanner.component.css'],
-	providers: [FileDatabase],
-	animations: [
-		trigger('openCloseLeftPanel', [
-			state('open', style({
-				width: '200px'
-			})),
-			state('closed', style({
-				width: '0'
-			})),
-			transition('open => closed', [
-				animate('0.5s')
-			]),
-			transition('closed => open', [
-				animate('0.5s')
-			]),
-		]),
-		trigger('openCloseRightPanel', [
-			state('open', style({
-				width: '264px'
-			})),
-			state('closed', style({
-				width: '0'
-			})),
-			transition('open => closed', [
-				animate('0.5s')
-			]),
-			transition('closed => open', [
-				animate('0.5s')
-			]),
-		]),
-	],
+	styleUrls: ['./scanner.component.css']
 })
 export class ScannerComponent implements OnInit {
-
-	noticeListe: NoticeModel[] = [];
-	noticeSelection: NoticeModel[] = [];
-	noticeElement: NoticeModel;
-	noticeAffiche: boolean = false;
+	scanListe:Array<string>;
+	noticeListe: NoticeModel[] = []; // Liste générale des notices à afficher / filter
+	noticeSelection: NoticeModel[] = []; // La liste des notices sélectionnées
+	noticeElement: NoticeModel; // La notice dont les infos sont affichées
+	noticeAffiche: boolean = false; // Affichers les infos rapides d'une notice
 	noticeFiltre: string = '';
 
-	leftPanelIsOpen = true;
-	rightPanelIsOpen = true;
+	leftPanelIsOpen:boolean = true;
+	rightPanelIsOpen:boolean = true;
 
-	nestedTreeControl: NestedTreeControl<FileNode>;
-	nestedDataSource: MatTreeNestedDataSource<FileNode>;
+	afficheDetailNotice:boolean = false; // Afficher le composant notice lors du clic sur un oeil (dans une notice)
 
-	constructor(public noticeService: NoticeService, private database: FileDatabase) { 
-		this.nestedTreeControl = new NestedTreeControl<FileNode>(this._getChildren);
-		this.nestedDataSource = new MatTreeNestedDataSource();
-
-		this.database.dataChange.subscribe(data => this.nestedDataSource.data = data);
-	}
-
-	hasNestedChild = (_: number, nodeData: FileNode) => !nodeData.type;
-
-	private _getChildren = (node: FileNode) => node.children;
+	constructor(public noticeService: NoticeService) { }
 
 	ngOnInit() {
 		this.noticeService.readNotices().subscribe(
@@ -177,21 +35,45 @@ export class ScannerComponent implements OnInit {
 			}
 		);
 	}
+	/**
+	 * Accéder aux notices d'une scan
+	 * @param dossier Dossier scanné pour afficher la liste des notices
+	 */
+	scanOnClick(dossier){
 
-	noticeOnClick($event, idNotice): void {
+	}
+	// Afficher le composant avec le détail des infos sur la notice
+	noticeAfficheDetail($event, idNotice): void {
+		// $event.preventDefault();
+		this.noticeElement = this.noticeListe[idNotice];
+		this.afficheDetailNotice = !this.afficheDetailNotice;
+	}
+	
+	// Choisir une notice et la mettre dans la liste
+	noticeOnChoisi($event, idNotice): void {
 		$event.preventDefault();
 		this.noticeElement = this.noticeListe[idNotice];
+		this.noticeListe[idNotice].selected = true;
 		if (this.noticeSelection.indexOf(this.noticeElement) == -1) {
 			this.noticeSelection.push(this.noticeElement);
 		}
+	}
+	// Afficher les infos d'une notice
+	noticeOnInfo($event, idNotice): void {
+		$event.preventDefault();
+		this.noticeElement = this.noticeListe[idNotice];
+		
 		this.noticeAffiche = true;
 	}
-
+	// Initialiser les sélections
 	noticeSelectionDump(): void {
 		this.noticeAffiche = false;
 		this.noticeSelection = [];
 	}
-
+	/**
+	 * Enlever une notice de la sélection
+	 * @param idNotice id de la notice à supprimer
+	 */
 	noticeSelectionRemove(idNotice): void {
 		for (let index = 0; index < this.noticeSelection.length; index++) {
 			let element = this.noticeSelection[index];
@@ -200,7 +82,28 @@ export class ScannerComponent implements OnInit {
 			}
 		}
 	}
-
+	/**
+	 * Ajouter toutes les notices à la sélection
+	 */
+	noticesToutesChoisies(): void {
+		this.noticeListe.forEach(
+			n =>{
+				n.selected = true;
+			}
+		)
+		this.noticeSelection = this.noticeListe;
+	}
+	/**
+	 * Ne sélectionner aucune notice
+	 */
+	noticesAucuneChoisie(): void {
+		this.noticeListe.forEach(
+			n =>{
+				n.selected = false;
+			}
+		)
+		this.noticeSelection = [];
+	}
 	toggleLeftPanel($event): void {
 		$event.preventDefault();
 		this.leftPanelIsOpen = !this.leftPanelIsOpen;
@@ -210,5 +113,11 @@ export class ScannerComponent implements OnInit {
 		$event.preventDefault();
 		this.rightPanelIsOpen = !this.rightPanelIsOpen;
 	}
-
+	/**
+	 * Masque la notice lorsqu'on clique sur le bouton pour la fermer
+	 * @param bool Booléen pour fermer la fenêtre (false)
+	 */
+	masqueNotice(bool:boolean){
+		this.afficheDetailNotice = bool;
+	}
 }
